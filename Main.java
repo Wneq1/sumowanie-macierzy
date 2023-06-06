19f58a2be15a5f065d11d824b29f4f25a828995e
Wneq1 <127328405+Wneq1@users.noreply.github.com>
2023-06-06 10:47:49 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+class Main {
+  public static void main(String[] args) {
+    int[][] macierz = {
+        {  1,  2,  3,  4 },
+        {  5,  6,  7,  8 },
+        {  9, 10, 11, 12 },
+        { 13, 14, 15, 16 }
+    };
+
+    for (int rzad = 0; rzad < macierz.length; rzad++) {
+      for (int kolumna = 0; kolumna < macierz[rzad].length; kolumna++) {
+        System.out.print(macierz[rzad][kolumna] + "\t");
+      }
+      System.out.println(); 
+    }
+    
+    int suma = 0;
+    for (int rzad = 0; rzad < macierz.length; rzad++) {
+      for (int kolumna = 0; kolumna < macierz[rzad].length; kolumna++) {
+        suma += macierz[rzad][kolumna];
+      }
+    }
+    
+    System.out.println("Suma elementów w macierzy: " + suma);
+  }
+}
